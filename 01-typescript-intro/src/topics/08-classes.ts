c4130716e77fc3d02f7c0ca5d955b42c122324df
Echizen22 <erixon.arce.justiniano@gmail.com>
2024-09-02 21:30:42 +0200

    Clases básicas

---
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -0,0 +1,18 @@
+
+
+export class Person {
+    public name: string;
+    private address: string;
+
+    constructor() {
+        this.name = 'Fernando';
+        this.address = 'New York';
+    }
+
+
+}
+
+
+const ironman = new Person();
+
+console.log(ironman);
