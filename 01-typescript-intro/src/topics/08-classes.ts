f0b3dc7db7f361994b7268fb2240fe31c0660241
Echizen22 <erixon.arce.justiniano@gmail.com>
2024-09-02 22:40:40 +0200

    Constructor de una clase

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -1,18 +1,18 @@
 
 
 export class Person {
-    public name: string;
-    private address: string;
+    // public name: string;
+    // private address: string;
 
-    constructor() {
-        this.name = 'Fernando';
-        this.address = 'New York';
-    }
+    constructor( 
+        public name: string, 
+        private address: string = 'No Address'
+    ) {}
 
 
 }
 
 
-const ironman = new Person();
+const ironman = new Person('Ironman', 'New York');
 
 console.log(ironman);
