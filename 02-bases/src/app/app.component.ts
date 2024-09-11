ab276edc8528140965d79ccd7ff7f38b6595c8cd
Echizen22 <erixon.arce.justiniano@gmail.com>
2024-09-11 22:48:09 +0200

    Funcionalidad del contador

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -8,17 +8,5 @@ export class AppComponent {
 
 
   public title: string = 'Hola Mundo';
-  public counter: number = 10;
-
-
-  increaseBy( value: number ):void {
-      this.counter += value;
-
-  }
-
-
-  resetCounter():void {
-    this.counter = 10;
-  }
 
 }
