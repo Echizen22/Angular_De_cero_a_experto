4c04a9e2fee570b7073f4c59e51e8e8527b929c6
Echizen22 <erixon.arce.justiniano@gmail.com>
2024-09-11 17:58:16 +0200

    Contador

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -11,5 +11,14 @@ export class AppComponent {
   public counter: number = 10;
 
 
+  increaseBy( value: number ):void {
+      this.counter += value;
+
+  }
+
+
+  resetCounter():void {
+    this.counter = 10;
+  }
 
 }
