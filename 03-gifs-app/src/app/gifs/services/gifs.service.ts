c8dbe0df53e9abb45c7bb8fe8d5333bbdf782562
Echizen22 <erixon.arce.justiniano@gmail.com>
2024-10-08 21:41:09 +0200

    Realizar una petición HTTP

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,13 +1,15 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
 
 
-  private _tagsHistory: string[] = [];
-  private apiKey: string = 'qAcgiqk8HA1GuMSgLaP3RXIHrqi2WUEM';
+  private _tagsHistory:  string[] = [];
+  private apiKey:        string = 'qAcgiqk8HA1GuMSgLaP3RXIHrqi2WUEM';
+  private serviceUrl:    string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -31,7 +33,18 @@ export class GifsService {
     if( tag.length === 0 ) return;
     this.organizeHistory(tag);
 
-    console.log(this.tagsHistory);
+    const params = new HttpParams()
+      .set('api_key', this.apiKey )
+      .set('limit', '10' )
+      .set('q', tag )
+
+    this.http.get(` ${ this.serviceUrl }/search`, { params } )
+      .subscribe( resp => {
+
+        console.log(resp);
+
+      });
+
 
   }
 
