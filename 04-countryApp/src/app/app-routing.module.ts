a792256b6abffecba79a2de3102234ce607111e4
Echizen22 <erixon.arce.justiniano@gmail.com>
2024-10-09 17:04:08 +0200

    Estructuras de páginas y SearchBox Component

--- 04-countryApp/src/app/app-routing.module.ts
+++ 04-countryApp/src/app/app-routing.module.ts
@@ -7,10 +7,10 @@ import { HomePageComponent } from './shared/pages/home-page/home-page.component'
 
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomePageComponent
-  },
+  // {
+  //   path: '',
+  //   component: HomePageComponent
+  // },
   {
     path: 'about',
     component: AboutPageComponent
@@ -21,11 +21,11 @@ const routes: Routes = [
   },
   {
     path: 'countries',
-    component: ContactComponent
+    loadChildren: () => import('./countries/countries.module').then( m => m.CountriesModule )
   },
   {
     path: '**',
-    redirectTo: ''
+    redirectTo: 'countries'
   }
 ];
 
