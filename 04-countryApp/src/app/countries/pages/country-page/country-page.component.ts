d4dcf7fa4242ca7753c3b54f5820513e5857d7db
Erixon Arce Justiniano <earce@odec.es>
2024-10-10 14:04:01 +0200

    switchMap - Refactorización

--- 04-countryApp/src/app/countries/pages/country-page/country-page.component.ts
+++ 04-countryApp/src/app/countries/pages/country-page/country-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -12,17 +13,21 @@ export class CountryPageComponent implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private countriesService: CountriesService,
+    private router: Router,
   ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params
-      .subscribe( ({ id }) => {
+      .pipe(
+        switchMap( ({ id }) => this.countriesService.searchCountryByAlphaCode( id ) )
+      )
+      .subscribe( country => {
+        if( !country ) {
+          return this.router.navigateByUrl('');
+        }
 
-        this.countriesService.searchCountryByAlphaCode( id )
-          .subscribe( country => {
-            console.log({ country });
-          });
-
+        console.log('TENEMOS UN PAÍS');
+        return;
       });
   }
 
