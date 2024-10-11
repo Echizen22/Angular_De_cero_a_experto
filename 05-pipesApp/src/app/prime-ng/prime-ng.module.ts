6253d315be391b8b0c5708ba870a15266cd0edf4
Echizen22 <erixon.arce.justiniano@gmail.com>
2024-10-11 22:57:50 +0200

    PrimeNG - MenuBar

--- 05-pipesApp/src/app/prime-ng/prime-ng.module.ts
+++ 05-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 
+import { MenubarModule } from 'primeng/menubar';
 import { MenuModule } from 'primeng/menu';
 
 
 @NgModule({
   exports: [
-    MenuModule
+    MenubarModule,
+    MenuModule,
   ]
 })
 export class PrimeNgModule { }
