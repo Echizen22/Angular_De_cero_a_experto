02951383bca69b3bfe9c43ff085e384abef91069
Echizen22 <erixon.arce.justiniano@gmail.com>
2024-10-12 13:57:42 +0200

    KeyValuePipe

--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -33,4 +33,11 @@ export class UncommonPageComponent {
     this.clients.shift();
   }
 
+  // KeyValue Pipe
+  public person = {
+    name: 'Fernando',
+    age: 36,
+    address: 'Ottawa, Canada',
+  }
+
 }
